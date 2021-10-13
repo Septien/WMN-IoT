7c8d9fdc5012945f13cc12a3422ad0a538f4ff07
Jose Septien <jaseptienh@gmail.com>
2021-10-13 17:18:02 -0500

    Add script for compiling and running only. No profiling.

---
+++ simple_build.py
@@ -0,0 +1,26 @@
+"""
+In this script, only compilation for linux and native riot is completed. No analysis
+or other tests are make here.
+"""
+
+import os
+
+def main():
+    cmd = "make clean LINUX=1 TEST=1"
+    os.system(cmd)
+    
+    cmd = "make all DEBUG=1 LINUX=1 TEST=1"
+    print(cmd)
+    os.system(cmd)
+    
+    cmd = "make all flash term RIOT=1 TEST=1"
+    print(cmd)
+    os.system(cmd)
+    
+    print("\n\nTesting for LINUX\n\n")
+    cmd = "bin/linux-x86_64/wmnlora"
+    print(cmd)
+    os.system(cmd)
+
+if __name__ == '__main__':
+    main()
