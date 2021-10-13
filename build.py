6062bd5bba16efcf58ae81c1dc5e2d2694dbfba1
Jose Septien <jaseptienh@gmail.com>
2021-10-13 17:17:34 -0500

    Complete script for automatically building/static code analysis/memory profiling, for both linux and riot.

--- build.py
+++ build.py
@@ -9,22 +9,37 @@ def main():
         Stack smash protector.
         For RIOT, emulate the target hardware (MCU) if available.
     """
+    # Compilation and analysis for linux
     # Compile with testing
     cmd = "make all DEBUG=1 LINUX=1 TEST=1"
     os.system(cmd)
     # Static analysis
-    cmd = "cpcheck bin/wmnlora"
+    cmd = "cppcheck . -D__LINUX__ --enable=all --includes-file=MAC/include --includes-file=MAC/include/testsinclude/ --language=c --std=c11 --cppcheck-build-dir=code-analysis 2> code-analysis/cpp_output.txt"
     os.system(cmd)
-    # Memory check wiht valgrin
-    cmd = "valgrind --tool=memcheck --leak-ckeck=yes bin/wmnlora"
+    # Memory leaks check with valgrin
+    cmd = "valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-output/memcheck-output.txt bin/linux-x86_64/wmnlora"
     os.system(cmd)
     # Valgrind with massif
-    cmd = "valgrin --tool=masif --stacks=yes bin/wmnlora"
+    cmd = "valgrind --tool=massif --stacks=yes --time-unit=B --massif-out-file=valgrind-output/massif-output.txt bin/linux-x86_64/wmnlora"
     os.system(cmd)
 
-
-    cmd = "BOARD=esp32-wroom-32 make all flash term PORT=/dev/ttyUSB0 RIOT=1"
+    # Compilation and analysis for RIOT
+    cmd = "make all RIOT=1 TEST=1"
     os.system(cmd)
+    # Static code analysis
+    cmd = "cppcheck . -D__RIOT__ --enable=all --includes-file=MAC/include --includes-file=MAC/include/testsinclude/ --language=c --std=c11 --cppcheck-build-dir=code-analysis 2> code-analysis/cpp_output.txt"
+    os.system(cmd)
+    # Compile codee for using with valgrind
+    cmd = "make -B clean all-valgrind"
+    os.system(cmd)
+    # Memory leaks check wwith valgrind
+    cmd = "valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-output/memcheck-output-riot.txt --track-origins=yes --fullpath-after=/home/phantom/CP_Systems/RTOS/RIOT/ --read-var-info=yes /home/phantom/CP_Systems/Implementations/WMNLoRa/bin/native/main_test.elf /dev/ttyACM0"
+    os.system(cmd)
+    # Valrgind with massif
+    cmd = "valgrind --tool=massif --stacks=yes --time-unit=B --massif-out-file=valgrind-output/massif-output-riot.txt bin/linux-x86_64/wmnlora --fullpath-after=/home/phantom/CP_Systems/RTOS/RIOT/ --read-var-info=yes /home/phantom/CP_Systems/Implementations/WMNLoRa/bin/native/main_test.elf /dev/ttyACM0"
+    os.system(cmd)
+    # Compile for the target board, flash, and execute
+    cmd = "BOARD=esp32-wroom-32 make all flash term RIOT=1 TEST=1 PORT=/dev/ttyUSB0"
 
 if __name__ == '__main__':
         main()
