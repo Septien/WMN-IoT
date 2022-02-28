a422531b334bf9eb49144275fd57ab3a8010fa94
Jose Septien <jaseptienh@gmail.com>
2022-02-28 16:17:23 -0600

    Add new parameter to config file.

--- ipc-queues/include/config_ipc.h
+++ ipc-queues/include/config_ipc.h
@@ -15,19 +15,23 @@
 #define IPC_QUEUES_CONFIG_HEADER
 
 #ifndef MAX_QUEUES
-#define MAX_QUEUES              100
+#define MAX_QUEUES                  100
 #endif
 
 #ifndef MAX_ELEMENTS_ON_QUEUE
-#define MAX_ELEMENTS_ON_QUEUE   100
+#define MAX_ELEMENTS_ON_QUEUE       100
 #endif
 
 #ifndef MAX_MESSAGE_SIZE
-#define MAX_MESSAGE_SIZE        100
+#define MAX_MESSAGE_SIZE            100
 #endif
 
 #ifndef QUEUE_SIZE
-#define QUEUE_SIZE              10
+#define QUEUE_SIZE                  10
+#endif
+
+#ifndef THREAD_STACKSIZE_DEFAULT
+#define THREAD_STACKSIZE_DEFAULT    2028
 #endif
 
 #endif      // IPC_QUEUES_CONFIG_HEADER
