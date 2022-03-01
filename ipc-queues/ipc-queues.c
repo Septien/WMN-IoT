6999f1a78aae7a1c380e1c8ad73b0ad5c1944a60
Jose Septien <jaseptienh@gmail.com>
2022-02-28 18:24:22 -0600

    Add the init function and the test code for the create function.

--- ipc-queues/ipc-queues.c
+++ ipc-queues/ipc-queues.c
@@ -2,10 +2,68 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <string.h>
 
 #include "ipc-queues.h"
 
+#ifdef __RIOT__
+extern char stack[MAX_QUEUES * THREAD_STACKSIZE_DEFAULT];
+extern msg_t queue[MAX_QUEUES * QUEUE_SIZE];
+#endif
+
+static IPC_Queues_t Queues;
+
 void init_queues(void)
 {
-    return;
+#ifdef __RIOT__
+    memset(stack, 0, sizeof(char) * MAX_QUEUES * THREAD_STACKSIZE_DEFAULT);
+    memset(queue, 0, sizeof(msg_t) * MAX_QUEUES * QUEUE_SIZE);
+#endif
+    for (int i = 0; i < MAX_QUEUES; i++)
+    {
+        Queue_t *q = &Queues.queues[i];
+        q->queue_id = 0;
+        q->queue_size = 0;
+        q->message_size = 0;
+        q->msgs_allow = 0;
+        q->msgs_on_queue = 0;
+#ifdef __RIOT__
+        q->stack = NULL;
+        q->queue = NULL;
+        Queues.free_stack = stack;
+        Queues.free_queue = queue;
+#endif
+        Queues.last_queue_id = 1;
+    }
 }
+
+uint32_t create_queue(size_t max_queue_size, size_t message_size, uint32_t msgs_allow, char *stack)
+{
+    (void) max_queue_size;
+    (void) message_size;
+    (void) msgs_allow;
+    (void) stack;
+    return 0;
+}
+
+#ifdef TESTING
+IPC_Queues_t *get_queues_pointer(void)
+{
+    IPC_Queues_t *p = &Queues;
+    printf("%p\n", (void *)p);
+    return p;
+}
+
+#ifdef __RIOT__
+char *_get_stack_pointer(void)
+{
+    return stack;
+}
+
+msg_t *_get_queue_pointer(void)
+{
+    return queue;
+}
+#endif  // __RIOT__
+
+#endif
--- ipc-queues/srcs_test_ipc/ipc-queues_Tests.c
+++ ipc-queues/srcs_test_ipc/ipc-queues_Tests.c
@@ -2,12 +2,114 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <assert.h>
 
 #include "ipc-queues_Tests.h"
 #include "ipc-queues.h"
+#include "config_ipc.h"
+
+void test_init_queues(void)
+{
+    init_queues();
+    IPC_Queues_t *Queues = get_queues_pointer();
+    printf("Queues: %p\n", (void *)Queues);
+#ifdef __RIOT__
+    char *stack = _get_stack_pointer();
+    msg_t *queue = _get_queue_pointer();
+    printf("Stack %p\n", (void *)stack);
+    printf("Queue: %p\n", (void *)queue);
+#endif
+    for (int i = 0; i < MAX_QUEUES; i++)
+    {
+        Queue_t *q = &Queues->queues[i];
+        assert(q->queue_id == 0);
+        assert(q->queue_size == 0);
+        assert(q->message_size == 0);
+        assert(q->msgs_allow == 0);
+        assert(q->msgs_on_queue == 0);
+#ifdef __RIOT__ 
+        assert(q->stack == NULL);
+        assert(q->queue == NULL);
+#endif
+    }
+    assert(Queues->last_queue_id == 1);
+#ifdef __RIOT__
+    assert(Queues->free_stack != NULL);
+    assert(Queues->free_queue != NULL);
+#endif
+}
+
+void test_create_queue(void)
+{
+    init_queues();
+    IPC_Queues_t *Queues = get_queues_pointer();
+    
+    uint32_t qid = 0;
+    uint32_t queue_size, msgs_allow, message_size;
+    char *stack = NULL;
+    queue_size = QUEUE_SIZE;
+    msgs_allow = rand() % queue_size;
+    message_size = MAX_MESSAGE_SIZE;
+
+    /**
+     * This function creates a new queue. It should update the free_stack and free_queue
+     * by the queue's requested size and the stack's predefined value (THREAD_STACKSIZE_DEFAULTS).
+     * The id of the queue, is one more than the index of the queue on the queues array.
+     * It should ckeck that there are queues available, and the size of the queue does not exceed
+     * the maximum allowed size.
+     * The funcion will do the following:
+     *      -Create a new queue id, from the variable last_queue_id, and increase by one unit
+     *      the latter.
+     *      -Update the pointers free_stack by THREAD_STACKSIZE_DEFAULT units, and free_queue by 
+     *      QUEUE_SIZE unit.
+     *      -On linux, call the mq_open function with the O_RDWR flag.
+     *      -On RIOT, call the msg_queue_init function.
+     * On the corresponding Queue_t structure, hold:
+     *      -The newly generated queue id.
+     *      -The queue size.
+     *      -The message_size.
+     *      -The allowed messages.
+     *      -On RIOT, hold the pointers to the correspoinding stack and queue positions.
+     * It should return:
+     *      -The queue's id.
+     *      -Pointer to the stack.
+     *      -The stack and queue pointers of the corresponding queue, should equal the previous
+     * value of free_stack and free_queue.
+     */
+#ifdef __RIOT__
+    // Get the last position from stack and queue.
+    char *last_stack = Queues->free_stack;
+    msg_t *last_queue = Queues->free_queue;
+#endif
+    qid = create_queue(queue_size, message_size, msgs_allow, stack);
+    assert(qid == Queues->last_queue_id - 1);
+    Queue_t *q = &Queues->queues[qid - 1];
+    assert(q->queue_size == queue_size);
+    assert(q->message_size == message_size);
+    assert(q->msgs_allow == msgs_allow);
+#ifdef __LINUX__
+    assert(stack == NULL);
+#endif
+#ifdef __RIOT__
+    assert(stack == last_stack);
+    assert(q->stack == last_stack);
+    assert(q->queue == last_queue);
+    assert(Queues->free_stack == last_stack + THREAD_STACKSIZE_DEFAULT);
+    assert(Queues->free_queue == last_queue + QUEUE_SIZE);
+#endif
+}
 
 void ipc_queues_tests(void)
 {
     printf("Testing the IPC Queues API.\n");
+
+    printf("Testing the init_queues function.\n");
+    test_init_queues();
+    printf("Test passed.\n");
+
+    printf("Testing the create_queue function.\n");
+    test_create_queue();
+    printf("Test passed.\n");
+
     return;
 }
