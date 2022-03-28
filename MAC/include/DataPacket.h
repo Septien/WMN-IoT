991153aa70df9bd836a83f413672466bfc863354
Jose Septien <jaseptienh@gmail.com>
2022-03-28 16:56:03 -0600

    Add documentation to DataPacket.h.

--- MAC/include/DataPacket.h
+++ MAC/include/DataPacket.h
@@ -31,11 +31,56 @@ typedef struct {
     ARRAY       data;
 }DataPacket_t;
 
+/**
+ * @brief Initializes the data packet structure. setting the data array to NULL, 
+ * the destination id to 0, the size to 0 and the type to -1.
+ * 
+ * @param pkt 
+ */
 void datapacket_init(DataPacket_t DOUBLE_POINTER pkt);
+
+/**
+ * @brief Destroy the packet en free any used memory.
+ * 
+ * @param pkt 
+ */
 void datapacket_destroy(DataPacket_t DOUBLE_POINTER pkt);
+
+/**
+ * @brief Creates the data packet with the given input parameters. Initialize the 
+ * data array to the give *size* and copies the data to the structure; stores the 
+ * type and destination id.
+ * 
+ * @param pkt 
+ * @param type 
+ * @param destination_id 
+ * @param data 
+ * @param size 
+ */
 void datapacket_create(DataPacket_t *pkt, int8_t type, uint16_t destination_id, ARRAY* data, uint8_t size);
+
+/**
+ * @brief Clear all the fields on the packet, and free any used memory. Set type to -1.
+ * 
+ * @param pkt 
+ */
 void datapacket_clear(DataPacket_t *pkt);
+
+/**
+ * @brief Get the representation of the packet in a string of bytes *byteString* (uint8_t.)
+ * 
+ * @param pkt 
+ * @param byteString 
+ */
 void datapacket_get_packet_bytestring(DataPacket_t *pkt, ARRAY* byteString);
+
+/**
+ * @brief Given a string of bytes *byteString*, construct the packet getting the data
+ * from such string. The data is stored on *byteString* as follows: <destination_id, type, data size, data>.
+ * 
+ * @param pkt 
+ * @param byteString 
+ */
 void datapacket_construct_from_bytestring(DataPacket_t *pkt, ARRAY* byteString);
 
 #endif  // DATAPACKET_H
