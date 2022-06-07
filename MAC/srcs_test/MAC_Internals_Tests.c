75dbe795c71af4d308dd172e70fcc5c2aa26d75f
Jose Septien <jaseptienh@gmail.com>
2022-06-07 12:24:25 -0500

    Add cUnit to mac internals tests.

--- MAC/srcs_test/MAC_Internals_Tests.c
+++ MAC/srcs_test/MAC_Internals_Tests.c
@@ -7,8 +7,9 @@
 
 #include "MAC_Internals.h"
 
-void test_MAC_internals_init(void)
-{
+#include "cUnit.h"
+
+struct mac_internals_data {
     MAC_Internals_t SINGLE_POINTER mac;
 #ifdef __LINUX__
     uint8_t radio;
@@ -16,83 +17,83 @@ void test_MAC_internals_init(void)
 #ifdef __RIOT__
     sx127x_t radio;
 #endif
+};
 
-    MAC_internals_init(&mac, &radio);
+void setup_mac_internals(void *arg)
+{
+    struct mac_internals_data *data = (struct mac_internals_data *)arg;
+    MAC_internals_init(&data->mac, &data->radio);
+}
+
+void teardown_mac_internal(void *arg)
+{
+    struct mac_internals_data *data = (struct mac_internals_data *)arg;
+    MAC_internals_destroy(&data->mac);
+}
+
+void test_MAC_internals_init(void *arg)
+{
+    struct mac_internals_data *data = (struct mac_internals_data *)arg;
+    MAC_Internals_t *mac = REFERENCE data->mac;
 
 #ifdef __LINUX__
     assert(mac != NULL);
     assert(mac->ctrlpkt != NULL);
 #endif
-    assert(ARROW(mac)selectedSlot == 0);
-    assert(ARROW(mac)transmitChannel == 0);
-    assert(ARROW(mac)receiveChannel == 0);
-    assert(ARROW(mac)cfChannel == 0);
-    assert(ARROW(mac)_collisionDetected == false);
-    assert(ARROW(mac)_is_internal_collision == false);
-    assert(ARROW(mac)_collisionSlot == 0);
-    assert(ARROW(mac)_collisionFrequency == 0);
-    assert(ARROW(mac)_cf_message_received == false);
-    assert(ARROW(mac)_packets_to_send_message == 0);
-    assert(ARROW(mac)_packets_to_send_control == 0);
-    assert(ARROW(mac)_last_send_control == 0);
-    assert(ARROW(mac)_number_packets_received == 0);
-
-    MAC_internals_destroy(&mac);
+    assert(mac->selectedSlot == 0);
+    assert(mac->transmitChannel == 0);
+    assert(mac->receiveChannel == 0);
+    assert(mac->cfChannel == 0);
+    assert(mac->_collisionDetected == false);
+    assert(mac->_is_internal_collision == false);
+    assert(mac->_collisionSlot == 0);
+    assert(mac->_collisionFrequency == 0);
+    assert(mac->_cf_message_received == false);
+    assert(mac->_packets_to_send_message == 0);
+    assert(mac->_packets_to_send_control == 0);
+    assert(mac->_last_send_control == 0);
+    assert(mac->_number_packets_received == 0);
 }
 
-void test_MAC_internals_destroy(void)
+void test_MAC_internals_destroy(void *arg)
 {
-    MAC_Internals_t SINGLE_POINTER mac;
-#ifdef __LINUX__
-    uint8_t radio;
-#endif
-#ifdef __RIOT__
-    sx127x_t radio;
-#endif
+    struct mac_internals_data *data = (struct mac_internals_data *)arg;
 
-    MAC_internals_init(&mac, &radio);
-    MAC_internals_destroy(&mac);
+    MAC_internals_destroy(&data->mac);
 #ifdef __LINUX__
-    assert(mac == NULL);
+    assert(data->mac == NULL);
 #endif
+    MAC_internals_init(&data->mac, &data->radio);
 }
 
-void test_MAC_internals_clear(void)
+void test_MAC_internals_clear(void *arg)
 {
-    MAC_Internals_t SINGLE_POINTER mac;
-#ifdef __LINUX__
-    uint8_t radio;
-#endif
-#ifdef __RIOT__
-    sx127x_t radio;
-#endif
+    struct mac_internals_data *data = (struct mac_internals_data *)arg;
+    MAC_Internals_t *mac = REFERENCE data->mac;
 
-    MAC_internals_init(&mac, &radio);
-
-    MAC_internals_clear(REFERENCE mac);
-    assert(ARROW(mac)selectedSlot == 0);
-    assert(ARROW(mac)transmitChannel == 0);
-    assert(ARROW(mac)cfChannel == 0);
-    assert(ARROW(mac)cfChannel == 0);
-    assert(ARROW(mac)_collisionDetected == false);
-    assert(ARROW(mac)_collisionSlot == 0);
-    assert(ARROW(mac)_collisionFrequency == 0);
-    assert(ARROW(mac)_cf_message_received == false);
-
-    MAC_internals_destroy(&mac);
+    MAC_internals_clear(mac);
+    assert(mac->selectedSlot == 0);
+    assert(mac->transmitChannel == 0);
+    assert(mac->cfChannel == 0);
+    assert(mac->cfChannel == 0);
+    assert(mac->_collisionDetected == false);
+    assert(mac->_collisionSlot == 0);
+    assert(mac->_collisionFrequency == 0);
+    assert(mac->_cf_message_received == false);
 }
 
 void executeTestsMACInternals(void)
 {
-    printf("Testing MAC_internals_init function.\n");
-    test_MAC_internals_init();
-    printf("Test passed.\n");
+    cUnit_t *tests;
+    struct mac_internals_data data;
 
-    printf("Testing MAC_internals_destroy function.\n");
-    test_MAC_internals_destroy();
-    printf("Test passed.\n");
+    cunit_init(&tests, &setup_mac_internals, &teardown_mac_internal, (void *)&data);
 
-    printf("Testing MAC_internals_clear function.\n");
-    test_MAC_internals_clear();
-    printf("Test passed.\n");
+    cunit_add_test(tests, &test_MAC_internals_init,     "MAC_internals_init\0");
+    cunit_add_test(tests, &test_MAC_internals_destroy,  "MAC_internals_destroy\0");
+    cunit_add_test(tests, &test_MAC_internals_clear,    "MAC_internals_clear\0");
+    
+    cunit_execute_tests(tests);
+
+    cunit_terminate(&tests);
 }
