7d052810303076e777bba85bb6620b5c1b1f3126
Jose Septien <jaseptienh@gmail.com>
2022-08-01 12:22:21 -0500

    Remove unnecessary macro.

--- MAC/MAC_StateMachine.c
+++ MAC/MAC_StateMachine.c
@@ -4,17 +4,6 @@
 
 #include "MCLMAC.h"
 
-#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
-#define BYTE_TO_BINARY(byte) \
-        (((byte) & 0x80) ? '1' : '0'), \
-        (((byte) & 0x40) ? '1' : '0'), \
-        (((byte) & 0x20) ? '1' : '0'), \
-        (((byte) & 0x10) ? '1' : '0'), \
-        (((byte) & 0x08) ? '1' : '0'), \
-        (((byte) & 0x04) ? '1' : '0'), \
-        (((byte) & 0x02) ? '1' : '0'), \
-        (((byte) & 0x01) ? '1' : '0')
-
 void mclmac_init_mac_state_machine(MCLMAC_t *mclmac)
 {
     assert(mclmac != NULL);
