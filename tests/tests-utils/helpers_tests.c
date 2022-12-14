b0266a49f405094375fc40146c01bb88928b0522
Jose Septien <jaseptienh@gmail.com>
2022-12-14 16:40:10 -0600

    Add test to execute the maximum number of nodes.

--- tests/tests-utils/helpers_tests.c
+++ tests/tests-utils/helpers_tests.c
@@ -12,7 +12,7 @@
 #include "printbinary.h"
 
 #ifdef __RIOT__
-char thread_stack[THREAD_STACKSIZE_MAIN];
+char thread_stack[MAX_NUMBER_NODES][THREAD_STACKSIZE_MAIN];
 #endif
 
 static void setup(void *arg)
@@ -50,7 +50,7 @@ bool test_execute_rema(void *arg)
     mtx_data = (mutex_t) MUTEX_INIT;
     mtx_req = (mutex_t) MUTEX_INIT;
     memset(thread_stack, 0, sizeof(thread_stack));
-    kernel_pid_t pid = thread_create(thread_stack, sizeof(thread_stack), THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_SLEEPING,
+    kernel_pid_t pid = thread_create(thread_stack[0], sizeof(thread_stack), THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_SLEEPING,
     execute_rema, (void *)&args, "REMA");
     thread_wakeup(pid);
     int ret = 0;
@@ -86,7 +86,7 @@ bool test_handle_get_nodeid_request(void *arg)
     mtx_data = (mutex_t) MUTEX_INIT;
     mtx_req = (mutex_t) MUTEX_INIT;
     memset(thread_stack, 0, sizeof(thread_stack));
-    kernel_pid_t pid = thread_create(thread_stack, sizeof(thread_stack), THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_SLEEPING,
+    kernel_pid_t pid = thread_create(thread_stack[0], sizeof(thread_stack[0]), THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_SLEEPING,
     execute_rema, (void *)&args, "REMA");
     thread_wakeup(pid);
 #endif
@@ -109,7 +109,56 @@ bool test_handle_get_nodeid_request(void *arg)
     memcpy(nid, args._node_id, 2*sizeof(uint64_t));
     _mutex_unlock(&mtx_data);
 
-    return memcmp(nid, id, 2*sizeof(uint64_t)) == 0;;
+    return memcmp(nid, id, 2*sizeof(uint64_t)) == 0;
+}
+
+bool test_create_n_threads(void *arg)
+{
+    (void) arg;
+    uint64_t id[2] = {1234567890, 9876543210};
+    data_t data[MAX_NUMBER_NODES] = {0};
+    request_t request[MAX_NUMBER_NODES];
+    mutex_t mtx_data[MAX_NUMBER_NODES], mtx_req[MAX_NUMBER_NODES];
+    args_t args[MAX_NUMBER_NODES];
+    uint32_t i;
+    for (i = 0; i < MAX_NUMBER_NODES; i++) {
+        args[i] = (args_t) {.data = &data[i], .request = &request[i],
+                            .mtx_data = &mtx_data[i], .mtx_req = &mtx_req[i]};
+        memcpy(args[i]._node_id, id, 2 * sizeof(uint64_t));
+    }
+#ifdef __LINUX__
+    pthread_t pid[MAX_NUMBER_NODES] = {0};
+    for (i = 0; i < MAX_NUMBER_NODES; i++) {
+        mtx_data[i] = (pthread_mutex_t) PTHREAD_MUTEX_INITIALIZER;
+        mtx_req[i] = (pthread_mutex_t) PTHREAD_MUTEX_INITIALIZER;
+        pthread_create(&pid[i], NULL, execute_rema, (void *)&args[i]);
+    }
+    usleep(100);
+#endif
+#ifdef __RIOT__
+    memset(thread_stack, 0, sizeof(thread_stack));
+    kernel_pid_t pid[MAX_NUMBER_NODES] = {0};
+    for (i = 0; i < MAX_NUMBER_NODES; i++) {
+        mtx_data[i] = (mutex_t) MUTEX_INIT;
+        mtx_req[i] = (mutex_t) MUTEX_INIT;
+        pid[i] = thread_create(thread_stack[i], sizeof(thread_stack[i]), THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_SLEEPING,
+                execute_rema, (void *)&args[i], "-");
+        thread_wakeup(pid[i]);
+    }
+#endif
+    for (i = 0; i < MAX_NUMBER_NODES; i++) {
+        while (_mutex_lock(&mtx_req[i]) != 1) ;
+        request[i] = STOP;
+        _mutex_unlock(&mtx_req[i]);
+#ifdef __LINUX__
+        pthread_join(pid[i], NULL);
+#endif
+#ifdef __RIOT__
+        thread_wakeup(pid[i]);
+#endif
+    }
+
+    return true;
 }
 
 void helpers_tests(void)
@@ -122,6 +171,7 @@ void helpers_tests(void)
 
     cunit_add_test(tests, &test_execute_rema,               "execution of rema protocol\0");
     cunit_add_test(tests, &test_handle_get_nodeid_request,  "node_id request\0");
+    cunit_add_test(tests, &test_create_n_threads,           "creation of n nodes\0");
 
     printf("Testing the helpers function.\n");
     cunit_execute_tests(tests);
