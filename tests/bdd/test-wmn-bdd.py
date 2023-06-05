32d19f8c17074763f8bd5345874b136f1621ce10
Jose Septien <jaseptienh@gmail.com>
2023-06-05 16:51:03 -0600

    Add the class that simulates the network's physical interface.

--- tests/bdd/test-wmn-bdd.py
+++ tests/bdd/test-wmn-bdd.py
@@ -1,13 +1,133 @@
 import unittest
-from wmn_bdd import WMN
+
+from wmn_bdd import WMN_Interface
+
 import paho.mqtt.client as mqtt
+from queue import Queue
+import time
 
 class test_wmn(unittest.TestCase):
-    def test_init(self):
-      wmn = WMN()
+    def setUp(self) -> None:
+        def on_message(client, userdata, message):
+            msg = message.payload.decode('utf-8')
+            topic = message.topic
+            if [msg, topic] != userdata:
+                raise ValueError("Incorrect message")
+        #
+        self.wmn = WMN_Interface()
+        self.client = mqtt.Client('test')
+        self.client.connect('localhost')
+        self.client.on_message = on_message
+        self.client.loop_start()
 
-      self.assertIsInstance(wmn.nodes, mqtt.Client)
-      self.assertTrue(wmn.nodes.is_connected())
+        while not self.client.is_connected(): continue
+        time.sleep(0.5)
+
+    def tearDown(self) -> None:
+        self.wmn.disconnect()
+        self.client.loop_stop()
+        self.client.disconnect()
+
+    def test_init(self):
+        wmn = WMN_Interface()
+
+        self.assertIsInstance(wmn.nodes, mqtt.Client)
+        self.assertTrue(wmn.nodes.is_connected())
+        self.assertIsInstance(wmn.to_receive, Queue)
+        self.assertIsNotNone(wmn.topics)
+        self.assertListEqual(wmn.topics, ['/nodes'])
+        self.assertTrue(self.wmn.nodes.is_connected())
+        wmn.disconnect()
+
+    def test_on_message(self):
+        m = 'Test message'
+        self.client.publish('/nodes', m)
+        time.sleep(0.5)
+
+        self.assertFalse(self.wmn.to_receive.empty())
+        message = self.wmn.to_receive.get()
+        self.assertListEqual(message, [m, '/nodes'])
+
+    def test_on_message_multiple_msgs(self):
+        m = 'Testing'
+        topic = '/nodes'
+        for _ in range(100):
+            self.client.publish(topic, m)
+        time.sleep(0.5)
+        self.assertFalse(self.wmn.to_receive.empty())
+        for _ in range(100):
+            msg = self.wmn.to_receive.get()
+            self.assertListEqual(msg, [m, topic])
+
+    def test_publish_to_send_none(self):
+        with self.assertRaises(ValueError):
+            self.wmn.message = None
+
+    def test_publish_to_send_len_no_2(self):
+        with self.assertRaises(ValueError):
+            self.wmn.message = ['topic']
+        
+        with self.assertRaises(ValueError):
+            self.wmn.message = ['topic', 'topic', 'topic']
+
+    def test_publish_no_topic(self):
+        msg = 'Test message'
+        topic = '/no/topic'
+
+        with self.assertRaises(ValueError):
+            self.wmn.message = [msg, topic]
+
+    def test_publish(self):
+        msg = 'Test message'
+        topic = '/nodes'
+        self.client.user_data_set([msg, topic])
+        self.client.subscribe(topic)
+        self.wmn.message = [msg, topic]
+        time.sleep(1)
+        recv = self.wmn.to_receive.get()
+        self.assertListEqual(recv, [msg, topic])
+        self.client.unsubscribe(topic=topic)
+
+    def test_message_none(self):
+        msg = self.wmn.message
+        self.assertIsNone(msg)
+
+    def test_message_recv(self):
+        msg = 'Test message'
+        topic = '/nodes'
+        self.wmn.message = [msg, topic]
+        time.sleep(0.5)
+
+        m = self.wmn.message
+        self.assertEqual(m, [msg, topic])
+
+    def test_subscribe_none(self):
+        with self.assertRaises(ValueError):
+            self.wmn.subscribe(None)
+        
+        with self.assertRaises(ValueError):
+            self.wmn.subscribe('')
+
+    def test_subscribe_one_topic(self):
+        topic = '/nodes/1'
+        self.wmn.subscribe(topic)
+        self.assertEqual(self.wmn.topics[-1], topic)
+
+    def test_subscribe_multiple_topics(self):
+        base_topic = '/nodes/'
+        topics = []
+        for i in range(100):
+            topics.append((base_topic + str(i), 1))
+        self.wmn.subscribe(topics)
+        for i in range(len(topics)):
+            self.assertEqual(topics[i][0], self.wmn.topics[i + 1])
+
+    def test_disconnect(self):
+        self.assertTrue(self.wmn.nodes.is_connected())
+        self.wmn.disconnect()
+        self.assertEqual(len(self.wmn.topics), 0)
+        self.assertFalse(self.wmn.nodes.is_connected())
+        self.assertFalse(self.wmn.nodes.is_connected())
 
 if __name__ == '__main__':
     unittest.main()
--- tests/bdd/wmn_bdd.py
+++ tests/bdd/wmn_bdd.py
@@ -1,21 +1,89 @@
 """
-This script implements a class for simulating the WMN, for making Behavioral tests.
+Implements a class that simulates the physical interface among the nodes on the network.
+This class acts as the 'central hub' for processing and distributing the messages in the
+network. It works as follows:
+
+It connects to a Mosquitto broker in order to receive messages from the nodes present in
+the network. Each node has its own topic which uses to publish and receive messages from
+the network. This class hears on each topic, and stores the message received for
+further processing by the upper class, which simulates the network. It also publishes
+or sends the messages to the nodes using their corresponding topics.
 """
 import paho.mqtt.client as mqtt
+from queue import Queue
 import time
+from typing import Any
 
-class WMN:
+class WMN_Interface:
+    """
+    Class for simulating the physical network's interface. It has the following members:
+        -nodes: the MQTT client for receiving/sending the messages.
+        -topics: a list of topics to which the client is connected.
+        -to_receive: a list of messages received from the broker.
+    """
     def __init__(self):
         def on_connect(client, userdata, flags, rc):
-            if rc == 0:
-                print("Connected, OK Returned rc = ", rc)
-            else:
+            if rc != 0:
                 print("Bad connection, Returned rc = ", rc)
+                client.is_connected = True
+        # Create client and connect to broker
         self.nodes = mqtt.Client('nodes')
         self.nodes.on_connect = on_connect
         self.nodes.connect('localhost')
+        self.nodes.on_message = self.on_message
         self.nodes.loop_start()
 
-        time.sleep(1)
+        while not self.nodes.is_connected(): continue;
 
         self.nodes.subscribe('/nodes')
+        self.topics = ['/nodes']
+        self.to_receive = Queue()
+
+    def on_message(self, client, userdata, message) -> None:
+        # Store the message for further processing
+        self.to_receive.put([message.payload.decode('utf-8'), message.topic])
+
+    def on_disconnect(self, client, userdata, rc) -> None:
+        if rc == 0:
+            self.nodes.loop_stop()
+
+    @property
+    def message(self) -> list:
+        """
+        Retrieves the last message received.
+        """
+        if self.to_receive.empty():
+            return None
+        return self.to_receive.get()
+    
+    @message.setter
+    def message(self, to_send : list = []) -> None:
+        """
+        Publish a new message. to_send is a list with the following format:
+            -to_send[0]: contains the message to send.
+            -to_send[1]: contains the message's topic.
+        """
+        if to_send is None or len(to_send) != 2:
+            raise ValueError("Must pass topic and message")
+        if to_send[1] not in self.topics:
+            raise ValueError("Topic doesn't exists.")
+
+        msg_info = self.nodes.publish(topic=to_send[1], payload=to_send[0])
+        msg_info.wait_for_publish(timeout=1)
+    
+    def subscribe(self, topic : Any) -> None:
+        if topic is None or len(topic) == 0:
+            raise ValueError("No topic received")
+        self.nodes.subscribe(topic)
+        if type(topic) is list:
+            for t in topic:
+                self.topics.append(t[0])
+        else:
+            self.topics.append(topic)
+
+    def disconnect(self) -> None:
+        for topic in self.topics:
+            self.nodes.unsubscribe(topic)
+        del self.topics
+        self.topics = []
+        self.nodes.disconnect()
